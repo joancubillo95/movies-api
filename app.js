4c8f1edbce37bf53ae368bdb1f0040bf6d5691e9
Joan Cubillo Arguedas <joan.cubillo95@gmail.com>
2026-06-19 12:09:22 -0600

    Add calls limiter

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ import { createMovieRouter } from "./routes/movies.router.js"
 import { MoviesModel } from "./models/movies.model.js"
 import 'dotenv/config'
 import { createHealthRouter } from "./routes/health.router.js"
+import { limiter } from "./middlewares/loginLimiter.js"
 
 const CreateApp = ({ movieModel }) => {
     const app = express()
     app.use(json())
     app.disable("x-powered-by")
+    app.use(limiter);
 
     app.use("/movies", createMovieRouter({ movieModel }))
     app.use("/", createHealthRouter())
