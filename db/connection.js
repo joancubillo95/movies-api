8a55e93676d016a9c5bc0dcde7553fad1a4f4ab6
Joan Cubillo Arguedas <joan.cubillo95@gmail.com>
2026-06-19 09:56:11 -0600

    Setup connection file

---
+++ db/connection.js
@@ -0,0 +1,27 @@
+import "dotenv/config";
+import sql from 'mssql';
+
+const config = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
+    options: {
+        encrypt: true,
+    },
+    pool: {
+        max: 2,
+        min: 0,
+        idleTimeoutMillis: 20000
+    }
+};
+
+const pool = new sql.ConnectionPool(config)
+const poolConnect = await pool.connect()
+
+const query = async (sqlString) => {
+    const result = await poolConnect.request().query(sqlString)
+    return result.recordset
+}
+
+export { poolConnect, query }
