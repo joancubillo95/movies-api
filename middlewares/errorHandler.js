3d348505de084be9351f9c5aa64f504ebb4cea82
Joan Cubillo Arguedas <joan.cubillo95@gmail.com>
2026-07-07 15:19:42 -0600

    Update error handler for dev environment

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -8,11 +8,12 @@ export function errorHandler(
   next
 ) {
   if (ENV === "development") {
-    // console.info("Stack trace: " + err.stack)
-    console.error(JSON.stringify(err.originalError))
+    console.info("Stack: " + err.stack)
+    console.error("Original error: " + err.originalError)
+    // Original Error object cannot be sent in res.json()
+    // because Error instances are not fully serializable.
     return res.status(err.statusCode).json({
-      message: err.message,
-      originalError: err.originalError
+      message: err.message + ". Check logs for additional information.",
     })
   } else {
     if (err.isOperational) {
