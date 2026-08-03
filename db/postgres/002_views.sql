22089a758e72f6d765ed83c474e4b7b8dee4f65d
Joan Cubillo Arguedas <joan.cubillo95@gmail.com>
2026-08-03 13:35:45 -0600

    Add postgres views

---
+++ db/postgres/002_views.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE VIEW vw_movies_with_genres AS
+SELECT
+    m.id,
+    m.title,
+    m.year,
+    m.director,
+    m.duration,
+    m.poster,
+    m.rate,
+    STRING_AGG(g.name, ',') AS genre
+FROM movie m
+INNER JOIN movie_genres mg
+    ON m.id = mg.movie_id
+INNER JOIN genre g
+    ON g.id = mg.genre_id
+GROUP BY
+    m.id,
+    m.title,
+    m.year,
+    m.director,
+    m.duration,
+    m.poster,
+    m.rate;
