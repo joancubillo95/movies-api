52073d46f7d696bef636d797c68594997fe2d9ef
Joan Cubillo Arguedas <joan.cubillo95@gmail.com>
2026-08-18 13:27:16 -0600

    implemented user module

---
+++ src/routes/users.router.js
@@ -0,0 +1,12 @@
+import { Router } from "express"
+import { UsersController } from "../controllers/users.controller.js"
+
+export const createUsersRouter = ({ usersRepository }) => {
+    const usersRouter = Router()
+
+    const usersController = new UsersController({ usersRepository })
+
+    usersRouter.get("/", usersController.getAll)
+
+    return usersRouter
+}
