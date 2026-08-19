135d89339ee18edf6cd8af9c8f7d70184e185fd8
Joan Cubillo Arguedas <joan.cubillo95@gmail.com>
2026-08-19 15:49:55 -0600

    updateusers module

--- src/repositories/pg/users.repository.js
+++ src/repositories/pg/users.repository.js
@@ -4,6 +4,19 @@ import { PostgresErrorMapper } from "../../utils/ErrorMappers/postgresErrorMappe
 
 export class UsersRepository extends BaseRepository {
 
+    getByUsername = async ({ username }) => {
+        try {
+            const pool = await this.database.getPool()
+            const [user] = (await pool.query("SELECT * FROM USERS WHERE UPPER(USERNAME) = $1", [username.toUpperCase()])).rows
+            if (!user) {
+                throw new Error("User not found!")
+            }
+            return user
+        } catch (error) {
+            throw this.errorMapper.map(error)
+        }
+    }
+
     getAll = async () => {
         try {
 
