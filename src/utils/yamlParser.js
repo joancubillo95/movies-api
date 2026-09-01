d9c99540cb2701b38aa1517d12c089d51519ac60
Joan Cubillo Arguedas <joan.cubillo95@gmail.com>
2026-09-01 10:10:37 -0600

    feat/ add yaml file parser

---
+++ src/utils/yamlParser.js
@@ -0,0 +1,18 @@
+import YAML from "yaml";
+import fs from "fs";
+
+const moviesSwagger = YAML.parse(
+    fs.readFileSync(
+        new URL("../swagger/movies.swagger.yaml", import.meta.url),
+        "utf8"
+    )
+);
+
+const authSwagger = YAML.parse(
+    fs.readFileSync(
+        new URL("../swagger/auth.swagger.yaml", import.meta.url),
+        "utf8"
+    )
+);
+
+export { moviesSwagger, authSwagger }
